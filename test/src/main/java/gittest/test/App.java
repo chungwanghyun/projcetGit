38415f8ced0b0001a8e4a502de84614ade1c6a59
user <user@192.168.0.190>
2020-05-29 14:51:21 +0900

    copy

--- test/src/main/java/gittest/test/App.java
+++ test/src/main/java/gittest/test/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!!!Branch!11123" );
+        System.out.println( "Hello World!!!Branch!11123" );
     }
 }
